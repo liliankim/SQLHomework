00ff179381298639b903a2d5a5e91ce9eb381129
liliankim <yejean831@gmail.com>
2022-10-01 15:06:58 -0700

    added inquirer prompt

--- index.js
+++ index.js
@@ -16,3 +16,22 @@ console.log(`you are connected!`)
 
 
 //this is for prompts
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'please choose from option below',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update an employee role'
+
+            ]
+        }
+    ])
+}
